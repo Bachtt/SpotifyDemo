589c3ce889ee03e3529c445c69a39b7620048f53
Bachtt <Bachtt107@gmail.com>
2022-08-09 19:37:39 +0700

    edit detail song

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Songs } from "../Context";
 
 export default function DetailSong() {
+  const { song } = useContext(Songs);
   return (
     <div className="col-span-1 p-3">
       <h2 className="text-cyan-500 font-bold">Now Playing</h2>
-      <h2 className="text-neutral-400 text-2xl">Sing me to sleep</h2>
+      <h2 className="text-neutral-400 text-2xl">{song.name}</h2>
       <div className="w-[200px] m-auto mt-10">
-        <img
-          className="w-full"
-          src="https://lucdia2.vn/loi-dich-sing-me-to-sleep/imager_37614.jpg"
-          alt="avatar"
-        />
+        <img className="w-full" src={song.links.images[0].url} alt="avatar" />
       </div>
       <div className="flex justify-evenly items-center mt-10 ">
         <img
           className="w-[50px] rounded-full"
-          src="https://lucdia2.vn/loi-dich-sing-me-to-sleep/imager_37614.jpg"
+          src={song.links.images[1].url}
           alt="avatar"
         />
-        <span className="text-xl text-white">Alan Walker</span>
+        <span className="text-xl text-white">{song.author}</span>
       </div>
     </div>
   );
